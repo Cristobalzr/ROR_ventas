334f661a9e2bad0bc3485410e6c89b02e47ffbf2
Cristobal <cristobalz@outlook.com>
2019-06-10 21:46:53 -0400

    second commit

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,4 +1,8 @@
 class SalesController < ApplicationController
   def new
+    
+  end
+  def create
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sales', to: 'sales#index'
   get 'sales/new'
+  get 'posts', to: 'sales#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
